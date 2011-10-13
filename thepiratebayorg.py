b29e984c965d6e8f37ffaaa9a9c19d0756dec829
Anton P. Linevich <anton@linevich.com>
2011-10-13 16:30:40 +0300

    description added

--- thepiratebayorg.py
+++ thepiratebayorg.py
@@ -13,14 +13,19 @@ def search(q, category):
     if not table:
         raise ValueError(u"No search results")
     for el_tr in table[0].xpath('tr'):
+        description = unicode(el_tr.xpath('td[2]/font[@class="detDesc"]/text()')[0]).encode('utf-8')
         yield {'torrent': el_tr.xpath('td[2]/a[1]/@href')[0],
                'seeders': el_tr.xpath('td[3]/text()')[0],
                'leachers': el_tr.xpath('td[4]/text()')[0],
+               'description': unicode(el_tr.xpath('td[2]/font[@class="detDesc"]/text()')[0]).encode('utf-8'),
                }
 
 
 if __name__ == "__main__":
     for row in search(sys.argv[1], '0/7/0'):
-        print u"%-3s %-3s %s" % (row['seeders'], row['leachers'],
-                row['torrent'])
+        print ("{se:>3} {le:>3} {desc} {url}".format(
+            se=row['seeders'],
+            le=row['leachers'],
+            desc=row['description'],
+            url=unicode(row['torrent'])) )
 
